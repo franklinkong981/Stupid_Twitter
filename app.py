855aafb41941b0ffe1f32ea439bef28bf6e8aab2
Franklin Kong <franklinkong981@gmail.com>
2024-05-04 14:27:04 -0700

    Added starter code and finished User model class

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///auth_demo'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+app.app_context().push()
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
